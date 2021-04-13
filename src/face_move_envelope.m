b08bb3a34aa8821c14688857b16becb0a355829b
LuzMtzL <71332518+LuzMtzL@users.noreply.github.com>
2021-04-13 18:29:34 -0500

    Changes 1

--- src/face_move_envelope.m
+++ src/face_move_envelope.m
@@ -3,8 +3,27 @@ close all;
 
 % create_face_vid('/home/deepcut/flowtron/results/rapsid0_sigma0.5.wavsid0_sigma0.5.wav_Rap_bpm=100_subpb=4_sylLen=1_Accapela.wav',...
 %     '/home/deepcut/flowtron/results/rap.avi');
+<<<<<<< Updated upstream
 create_face_vid('sid0_sigma0.5.wav_Rap_bpm=100_subpb=4_sylLen=0.5_Accapela.wav','rap.avi');
 mix_beat('sid0_sigma0.5.wav_Rap_bpm=100_subpb=4_sylLen=0.5_Accapela.wav','beat1.wav')
+=======
+create_face_vid('sid0_sigma0.5.wav_Rap_bpm=100_subpb=4_sylLen=0.5_Accapela.wav','rap.mp4');
+mix_beat('sid0_sigma0.5.wav_Rap_bpm=100_subpb=4_sylLen=0.5_Accapela.wav','beat1.wav')
+
+command1 = 'ffmpeg -i "C:/Users/luzma/PycharmProjects/pythonProject/Senior Design/rap.mp4" -i "C:/Users/luzma/PycharmProjects/pythonProject/Senior Design/mix.wav" -c:v copy -c:a aac "C:/Users/luzma/PycharmProjects/pythonProject/Senior Design/done.mp4" -y';
+% command2 = 'ffmpeg -i C:/Users/luzma/PycharmProjects/pythonProject/Senior Design/done.avi C:/Users/luzma/PycharmProjects/pythonProject/Senior Design/done.mp4 -y';
+system(command1);
+% system(command2);
+
+function mix_beat(rapFile, beatFile)
+    [rap, FSr] = audioread(rapFile);
+    [beat, FSb] = audioread(beatFile);
+    rap = resample(rap,441,160);
+    mix = rap + beat(1:length(rap)) / 2;
+    audiowrite('mix.wav',mix,FSb)
+end
+
+>>>>>>> Stashed changes
 
 command1 = 'ffmpeg -i /home/deepcut/deepcut/src/rap.avi -i /home/deepcut/deepcut/src/mix.wav -c:v copy -c:a aac /home/deepcut/deepcut/src/done.avi -y';
 command2 = 'ffmpeg -i /home/deepcut/deepcut/src/done.avi /home/deepcut/deepcut/src/done.mp4 -y';
